1df306dfd31d5ad1687548cad7564cb8e5a7e8f1
tim cyber securtiy <71566130+janda354311@users.noreply.github.com>
2024-04-02 16:25:59 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<div style="display:none;">
+
+<a href="https://kknreguler.unsam.ac.id/tinymce/" rel="dofollow">Slot Gacor</a>
+<a href="https://divif2.kostrad.mil.id/-/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://divif2.kostrad.mil.id/store/server-thailand/" rel="dofollow">Slot Gacor</a>
+<a href="https://ptsp.halal.go.id/store/server-thailand/" rel="dofollow">Slot Gacor</a>
+<a href="https://ptsp.halal.go.id/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://ptsp.halal.go.id/pelatihan/" rel="dofollow">Slot Gacor</a>
+<a href="https://stih-painan.ac.id/store/slot-thailand/" rel="dofollow">Slot Gacor</a>
+<a href="https://dosen.stih-painan.ac.id/" rel="dofollow">Slot Gacor</a>
+<a href="https://tka-online.kemnaker.go.id/pdf/" rel="dofollow">Slot Gacor</a>
+<a href="https://tka-online.kemnaker.go.id/img/" rel="dofollow">Slot Gacor</a>
+<a href="http://103.47.60.38/icons/sthailand/" rel="dofollow">Slot Gacor</a>
+<a href="http://103.47.60.38/sgacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://sp.sidomuncul.co.id/sp_mon/s-gacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://elapor.merauke.go.id/login/home/" rel="dofollow">Slot Gacor</a>
+<a href="https://komisietik.fk.ugm.ac.id/images/pdf/" rel="dofollow">Slot Gacor</a>
+<a href="https://inkadha.ac.id/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://mbkm.ilkom.unsri.ac.id/public/sgacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://upertis.ac.id/-/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://arsip-ppds.usu.ac.id/-/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://pro.unitri.ac.id/js/slotgacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://bpsdm.sultengprov.go.id/licensing/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
+<a href="https://bpsdm.sultengprov.go.id/documentation/sgacor/" rel="dofollow">Slot Gacor</a>
+<a href="https://diskoperindag.pemalangkab.go.id/assets/-/slot/" rel="dofollow">Slot Gacor</a>
+<a href="https://sttpk-medan.ac.id/slot-hari-ini/" rel="dofollow">Slot Gacor</a>
